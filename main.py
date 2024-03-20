8fb77a902e9c8c9e29be4f2826f5d77cdda3695d
Ventinos <gustavo.ventino01@gmail.com>
2024-03-20 16:48:03 -0300

    fixing some stuff

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-import random
+from random import randint
+from time import time
 
+# auxiliar methods:
 def max(a, b):
     if a > b:
         return a
     return b
 
+# top-bottom naive recursive implementation:
 def cut_rod(p, n):
     if n == 0:
         return 0
 
     q = -1
     for i in range(1, n+1):
-        q = max(q, p[i] + cut_rod(p, n-i))
+        if(i < len(p)):
+            q = max(q, p[i] + cut_rod(p, n-i))
+        else:
+            q = max(q, cut_rod(p, i) + cut_rod(p, n-i))
 
     return q
 
+# top-down with memoization implementation:
 def memoized_cut_rod(p, n):
-    r = [-1 for _ in range(0,n+1)]
+    r = [-1]*(n+1)
     return memoized_cut_rod_aux(p, n, r)
 
 def memoized_cut_rod_aux(p, n, r):
@@ -28,30 +35,38 @@ def memoized_cut_rod_aux(p, n, r):
     else:
         q = -1
         for i in range(1, n+1):
-            q = max(q, p[i] + memoized_cut_rod_aux(p, n-i, r))
+            if(i < len(p)):
+                q = max(q, p[i] + memoized_cut_rod_aux(p, n-i, r))
+            else:
+                q = max(q, memoized_cut_rod_aux(p, i, r) + memoized_cut_rod_aux(p, n-i, r))
 
     r[n] = q
     return q
 
+# bottom-up implementation:
 def bottom_up_cut_rod(p, n):
-    r = [i for i in range(0, n+1)]
+    r = list(range(0, n+1))
 
     for j in range(1, n+1):
         q = -1
         for i in range(1, j+1):
-            q = max(q, p[i] + r[j - i])
+            if(i < len(p)):
+                q = max(q, p[i] + r[j - i])
+            else:
+                q = max(q, r[i] + r[j - i])
         r[j] = q
 
     return r[n]
 
+# reconstruction of the optimal solution:
 def extended_bottom_up_cut_rod(p, n):
-    r = [i for i in range(0, n+1)]
-    s = [i for i in range(1, n+2)]
+    r = list(range(0, n+1))
+    s = list(range(1, n+2))
 
     for j in range(1, n+1):
         q = -1
         for i in range(1, j+1):
-            if q < p[i] + r[j - i]:
+            if(q < p[i] + r[j - i]):
                 q = p[i] + r[j - i]
                 s[j] = i
         r[j] = q
@@ -66,17 +81,17 @@ def print_cut_rod_solution(p, n):
         n = n - s[n]
 
 def main():
-    #TODO: ajustar todas os metodos para aceitar n maiores que len(p)
+    #TODO: arrumar cut_rod(p, n)
     p = [0, 1, 5, 8, 9, 10, 17, 20, 24, 30]
-    #Esse eh um jeito de criar uma tabela desafiadora de precos:
-    p1 = [random.randint(0,10000) for _ in range(0,11)]
-    p1[0] = 0
+    p1 = [0, 2, 1, 3]
+    rand_p = [randint(0,10000) for _ in range(0,50)]
+    rand_p[0] = 0
+    #for n in range(0, 1001):
+        #start = time()
+    print(bottom_up_cut_rod(p, 4))
+        #end = time()
+        #print(end - start)
 
-    n = 6
-    print(cut_rod(p, n))
-    print(memoized_cut_rod(p, n))
-    print(bottom_up_cut_rod(p, n))
-    print(extended_bottom_up_cut_rod(p, n))
 
 if __name__=='__main__':
     main()
