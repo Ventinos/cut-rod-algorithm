74435199fa98b7c807c32f9a0e150e4dd82a219b
Ventinos <gustavo.ventino01@gmail.com>
2024-03-21 12:39:01 -0300

    going

--- generate_data.py
+++ generate_data.py
@@ -8,18 +8,19 @@ def main():
     #print_tests(p, 'bottom_up_p_book.csv', bottom_up_cut_rod)
 
     # Random table:
-    for n in [50, 150, 250 , 500]:
+    for n in [5, 10, 20, 30]:
         rand_p = [randint(0,10000) for _ in range(0,n)]
         rand_p[0] = 0
+
+        with open("used_ps.txt", "a") as f:
+            print(n, file=f, end=";")
+            print(rand_p, file=f)
+
 
         print_tests(rand_p, 'naive_p_rand' + str(n) + '.csv', cut_rod)
         print_tests(rand_p, 'bottom_up_p_rand' + str(n) + '.csv', bottom_up_cut_rod)
 
         length_time_comparisson_plot('naive_p_rand' + str(n) + '.csv', 'bottom_up_p_rand' + str(n) + '.csv')
-
-        with open("used_ps.txt", "a") as f:
-            print(n, file=f, end=";")
-            print(rand_p, file=f)
 
 if __name__=='__main__':
     main()
